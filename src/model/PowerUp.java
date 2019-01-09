471d587476ac433098cd5625aeda91e64847035b
LKreuz <lil.kreuzburg@gmail.com>
2019-01-09 12:45:58 +0100

    anfang item und powerup

---
+++ src/model/PowerUp.java
@@ -0,0 +1,30 @@
+package model;
+
+import akkgframework.model.fundamental.GraphicalObject;
+import akkgframework.view.DrawTool;
+
+public class PowerUp extends GraphicalObject {
+    private int strength;
+    public PowerUp(){
+        jump();
+
+
+
+    }
+    public  void draw(DrawTool drawTool) {
+        drawTool.setCurrentColor(59,59,59,255);
+        drawTool.drawFilledCircle(x,y,60);
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int amount) {
+        strength = strength+ amount;
+    }
+    private void jump(){
+        x=Math.random()*1600;
+        y= Math.random()*1600;
+    }
+}
---
+++ src/model/item.java
@@ -0,0 +1,51 @@
+package model;
+
+import akkgframework.model.fundamental.GraphicalObject;
+import akkgframework.view.DrawTool;
+
+public class item extends GraphicalObject {
+    private int colorNumber;
+    private double timer;
+
+    public item(int colorNumber) {
+        this.colorNumber=colorNumber;
+        x= Math.random()*1600;
+        y= Math.random()*1600;
+        height= 40;
+        width=40;
+        timer=0;
+
+
+    }
+
+    public void draw(DrawTool drawTool) {
+        switch (colorNumber) {
+            case 1: drawTool.setCurrentColor(255, 215, 0, 255);
+            break;
+            case 2: drawTool.setCurrentColor(255, 44, 44, 255);
+            break;
+            case 3: drawTool.setCurrentColor(191, 61, 255, 255);
+            break;
+            case 4: drawTool.setCurrentColor(34, 139, 34, 255);
+            break;
+            case 5: drawTool.setCurrentColor(72, 118, 255, 255);
+            break;
+
+        }
+        drawTool.drawFilledRectangle(x,y,width,height);
+    }
+    public int getColorNumber() {
+        return colorNumber;
+    }
+    private void jump(){
+        x= Math.random()*1600;
+        y= Math.random()*1600;
+    }
+    public void update(double dt){
+        timer= timer+1*dt;
+        if( timer >=10){
+            jump();
+        }
+
+    }
+}
